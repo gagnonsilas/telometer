b648b293a0faf09f91831817c9bae8ff4e202d9f
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-08-09 12:55:18 -0400

    add skeleton dear imgui zig application

--- dashboard/build.zig
+++ dashboard/build.zig
@@ -3,76 +3,90 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "dashboard",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const imgui_dep = b.dependency("imgui", .{});
+    const dear_bindings_dep = b.dependency("dear_bindings", .{});
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const dear_bindings_py = dear_bindings_dep.path("dear_bindings.py");
+
+    const imgui_lib = b.addStaticLibrary(.{ .name = "imgui_zig", .target = target, .optimize = optimize });
+
+    if (std.fs.openFileAbsolute(b.path("generated/cimgui.h").getPath(b), .{}) == error.FileNotFound) {
+        const generate_main_bindings = b.addSystemCommand(&.{"python"});
+        generate_main_bindings.addFileArg(dear_bindings_py);
+        generate_main_bindings.addArgs(&.{ "-o", "generated/cimgui" });
+        generate_main_bindings.addFileArg(imgui_dep.path("imgui.h"));
+
+        imgui_lib.step.dependOn(&generate_main_bindings.step);
+    }
+
+    if (std.fs.openFileAbsolute(b.path("generated/cimgui_impl_glfw.h").getPath(b), .{}) == error.FileNotFound) {
+        const generate_glfw_backend_bindings = b.addSystemCommand(&.{"python"});
+        generate_glfw_backend_bindings.addFileArg(dear_bindings_py);
+        generate_glfw_backend_bindings.addArg("--backend");
+        generate_glfw_backend_bindings.addArg("--imconfig-path");
+        generate_glfw_backend_bindings.addFileArg(imgui_dep.path("imconfig.h"));
+        generate_glfw_backend_bindings.addArgs(&.{ "-o", "generated/cimgui_impl_glfw" });
+        generate_glfw_backend_bindings.addFileArg(imgui_dep.path("backends/imgui_impl_glfw.h"));
+
+        imgui_lib.step.dependOn(&generate_glfw_backend_bindings.step);
+    }
+
+    if (std.fs.openFileAbsolute(b.path("generated/cimgui_impl_opengl3.h").getPath(b), .{}) == error.FileNotFound) {
+        const generate_opengl_backend_bindings = b.addSystemCommand(&.{"python"});
+        generate_opengl_backend_bindings.addFileArg(dear_bindings_py);
+        generate_opengl_backend_bindings.addArg("--backend");
+        generate_opengl_backend_bindings.addArg("--imconfig-path");
+        generate_opengl_backend_bindings.addFileArg(imgui_dep.path("imconfig.h"));
+        generate_opengl_backend_bindings.addArgs(&.{ "-o", "generated/cimgui_impl_opengl3" });
+        generate_opengl_backend_bindings.addFileArg(imgui_dep.path("backends/imgui_impl_opengl3.h"));
+
+        imgui_lib.step.dependOn(&generate_opengl_backend_bindings.step);
+    }
+
+    imgui_lib.addCSourceFiles(.{ .root = b.path("generated"), .files = &.{ "cimgui.cpp", "cimgui_impl_glfw.cpp", "cimgui_impl_opengl3.cpp" } });
+    imgui_lib.addCSourceFiles(.{ .root = imgui_dep.path(""), .files = &.{ "imgui.cpp", "imgui_demo.cpp", "imgui_draw.cpp", "imgui_tables.cpp", "imgui_widgets.cpp" } });
+    imgui_lib.addCSourceFiles(.{ .root = imgui_dep.path("backends"), .files = &.{ "imgui_impl_glfw.cpp", "imgui_impl_opengl3.cpp" } });
+
+    imgui_lib.addIncludePath(b.path("generated"));
+    imgui_lib.addIncludePath(imgui_dep.path(""));
+    imgui_lib.addIncludePath(imgui_dep.path("backends"));
+
+    imgui_lib.linkLibCpp();
+    imgui_lib.linkLibC();
+    imgui_lib.linkSystemLibrary("glfw");
+
+    b.installArtifact(imgui_lib);
 
     const exe = b.addExecutable(.{
-        .name = "dashboard",
+        .name = "dear_imgui_zig",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    exe.linkLibC();
+    exe.linkLibrary(imgui_lib);
+    exe.linkSystemLibrary("GL");
+    exe.linkSystemLibrary("glfw");
+
+    exe.addIncludePath(imgui_dep.path(""));
+    exe.addIncludePath(b.path("generated"));
+
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -82,10 +96,6 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
