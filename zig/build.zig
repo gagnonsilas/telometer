aa21106a15f8bd044b3ea0a841d61660d3483c28
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-09-28 01:08:11 -0400

    Copy cpp implementation to zig

--- zig/build.zig
+++ zig/build.zig
@@ -24,6 +24,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    lib.addIncludePath(b.path("../src/Telometer.h"));
+
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
--- zig/src/Telometer.zig
+++ zig/src/Telometer.zig
@@ -1,94 +1,69 @@
 const std = @import("std");
-const telometer = @cImport({@cInclude("../../src/Telometer.h");});
+const telometer = @cImport({
+    @cInclude("Telometer.h");
+});
 
 const PacketState = telometer.TelometerPacketState;
-const Data = telometer.Data;
+const Data = telometer.TelometerData;
 
 pub fn TelometerInstance(comptime Backend: type, comptime PacketStruct: type) type {
-  return struct {
-    const Self = @This();
-    backend: Backend,
-    next_packet: u8 = 0,
-    packet_struct: [@sizeOf(PacketStruct) / @sizeof(Data)] Data,
-    
-    
-    pub fn init(allocator: std.mem.Allocator, backend: Backend, packet_struct: PacketStruct) Self {
-      
-      inline for (packet_struct, 0..) |packet, i| {
-        packet.pointer = @pointerCast(allocater.alloc(u8, packet.size));
-        packet.
-      }
+    return struct {
+        const Self = @This();
+        backend: Backend,
+        next_packet: u8 = 0,
+        count: usize,
+        packet_struct: PacketStruct,
 
-      return Self{
-        .backend = backend,
-        .packet_struct = packet_struct,
-      };
-    }
+        pub fn init(allocator: std.mem.Allocator, backend: Backend, packet_struct: PacketStruct) Self {
+            inline for (@typeInfo(PacketStruct).Struct.fields) |packet| {
+                @field(packet_struct, packet.name).pointer = @ptrCast(allocator.alloc(u8, @field(packet_struct, packet.name).size));
+            }
 
-    pub fn update(instance: Self) void {
-      instance.backend.backendUpdateBegin();
-      for (0..count) |i| {
-        var currentId : usize = ((i + instance.next_packet) % instance.packet_struct.len);
-
-        var packet: Data = instance.packet_struct[currentId];
-
-        if (packet.state == TelometerSent or packet.state == TelometerReceived) {
-          continue;
+            return Self{
+                .backend = backend,
+                .packet_struct = packet_struct,
+                .count = @typeInfo(PacketStruct).Struct.fields.len,
+            };
         }
 
-        if (packet.size + sizeof(packetID) >
-            instance.backend.availableForWrite()) {
-          instance.next_packet = currentId;
-          break;
+        pub fn update(self: Self) void {
+            for (0..self.count) |i| {
+                const current_id = (self.next_packet + i) % self.count;
+
+                var packet = @field(self.packet_struct, @typeInfo(PacketStruct).Struct.fields[current_id].name);
+
+                if (packet.state == .TelometerSent or packet.state == .TelometerRecieved) {
+                    continue;
+                }
+
+                if (!self.backend.writePacket(packet)) {
+                    self.next_packet = current_id;
+                    break;
+                }
+
+                packet.state = .TelometerSent;
+
+                var id: usize = undefined;
+                while (self.backend.getNextId(&id)) {
+                    if (id >= self.count) {
+                        std.log.err("Invalid header\n", .{});
+                        continue;
+                    }
+
+                    packet = @field(self.packet_struct, @typeInfo(PacketStruct).Struct.fields[id].name);
+
+                    if (packet.state == .TelometerLockedQueued) {
+                        self.backend.read(null, packet.size);
+                        continue;
+                    }
+
+                    self.backend.read(@ptrCast(packet.pointer), packet.size);
+
+                    packet.state = .TelometerReceived;
+                }
+            }
+
+            self.backend.update();
         }
-
-        instance.backend.writePacket(packet);
-
-        packet.state = TelometerSent;
-      }
-
-      
-      while (instance.backend.getNextID()) |id| {
-    
-        if (id >= instance.count) {
-          debug("invalid header\n");
-          continue;
-        }
-
-        var packet: Data = instance.packet_struct[id];
-
-        if(packet.state == TelometerLockedQueued) {
-          uint8_t* trashBin = (uint8_t*)alloca(packet.size);
-          instance.backend.read(trashBin, packet.size);
-          continue;
-        }
-
-        instance.backend.read(packet.pointer, packet.size);
-
-        packet.state = TelometerReceived;
-      }
-
-      instance.backend.updateEnd();
-    }
-
-  };
+    };
 }
-
-
-// Log a value for a specific log ID
-pub fn sendValue(packet: Data, data:*void) void {
-  @memcpy(@as([packet.size]u8, packet.pointer), data);
-  packet.state = TelometerQueued;
-}
-
-// Log a data pointer for a specific log ID
-pub fn initPacket(packet: Data, data: *anyopaque) void {
-  free(packet.pointer);
-  packet.pointer = data;
-  packet.state = TelometerQueued;
-}
-
-// Mark a packet for update
-pub fn sendPacket(packet: Data) void { packet.state = TelometerQueued; }
-
-
