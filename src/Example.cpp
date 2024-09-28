aec3ec7a1af607f6cf1f16f6f6eb294d6a8810d5
gagnonsilas <gagnon.silas@gmail.com>
2024-09-27 23:13:59 -0400

    add example.cpp

---
+++ src/Example.cpp
@@ -0,0 +1,25 @@
+#include "Example.h"
+#include "../cpp/TelometerImpl.h"
+#include <iterator>
+
+
+struct TelemetryPackets packets;
+
+Telometer::TelometerInstance telemetry;
+
+int main() {
+  telemetry.backend->init();
+  return 0;
+}
+
+void test() {
+  packets.robotPos.pointer = 
+
+  telemetry.backend->
+
+  Telometer::initPacket()
+
+  packets.robotPos.state = TelometerSent;
+  
+  
+}
