47870d472268a2e948c237640826c7a742fda358
Gavin Ford <gavin4d05@gmail.com>
2025-03-22 21:13:34 +1300

    BLE backend now gracefully handles disconnects

--- dashboard/src/backend.zig
+++ dashboard/src/backend.zig
@@ -36,7 +36,7 @@ const BLEBackend: type = struct {
 
     var thread: std.Thread = undefined;
     var connected: bool = false;
-    var dev_path: [*c]const u8 = "";
+    var dev_path: [*c]const u8 = "/org/bluez/hci0/dev_F4_12_FA_F0_91_2A";
     var buf_error: DBusError = undefined;
     var dbus_error: *dbus.DBusError = @ptrCast(&buf_error);
     var connection: ?*dbus.DBusConnection = null;
@@ -46,8 +46,9 @@ const BLEBackend: type = struct {
     var msgWrite: ?*dbus.DBusMessage = null;
 
     fn check(check_error: ?*dbus.DBusError) void {
-        if (check_error != null and check_error != undefined and dbus.dbus_error_is_set(check_error) != 0) {
-            std.debug.print("DBus Error {s}\n", .{buf_error.message});
+        if (dbus.dbus_error_is_set(check_error) != 0) {
+            std.debug.print("DBus Error {s}", .{buf_error.message});
+            dbus.dbus_error_free(check_error);
         }
     }
 
@@ -111,18 +112,40 @@ const BLEBackend: type = struct {
         return "";
     }
 
-    fn connect(device_path: [*c]const u8) bool {
-        msgQuery = dbus.dbus_message_new_method_call(bluez, device_path, device, "Connect");
-        _ = dbus.dbus_connection_send_with_reply_and_block(connection, msgQuery, 10000, dbus_error);
-        check(dbus_error);
-        dbus.dbus_message_unref(msgQuery);
+    fn isConnected() bool {
+        var message: ?*dbus.DBusMessage = null;
+        var reply: ?*dbus.DBusMessage = null;
+        message = dbus.dbus_message_new_method_call(bluez, dev_path, "org.freedesktop.DBus.Properties", "Get");
+        const variable: [*c]const u8 = "Connected";
+        _ = dbus.dbus_message_append_args(message, dbus.DBUS_TYPE_STRING, &device, dbus.DBUS_TYPE_STRING, &variable, dbus.DBUS_TYPE_INVALID);
+        reply = dbus.dbus_connection_send_with_reply_and_block(connection, message, 1000, dbus_error);
+        dbus.dbus_message_unref(message);
         if (dbus.dbus_error_is_set(dbus_error) != 0) {
-            std.debug.print("Connection error", .{});
+            dbus.dbus_error_free(dbus_error);
             return false;
         }
-        std.time.sleep(std.time.ns_per_s * 4); // TODO: check that we are actually connected
-        connected = true;
+        var iter: dbus.DBusMessageIter = undefined;
+        var sub: dbus.DBusMessageIter = undefined;
+        var result: u8 = 0;
+        _ = dbus.dbus_message_iter_init(reply, &iter);
+        dbus.dbus_message_iter_recurse(&iter, &sub);
+        dbus.dbus_message_iter_get_basic(&sub, &result);
+        dbus.dbus_message_unref(reply);
+        return result != 0;
+    }
+
+    fn connect(device_path: [*c]const u8) bool {
+        msgQuery = dbus.dbus_message_new_method_call(bluez, device_path, device, "Connect");
+        msgReply = dbus.dbus_connection_send_with_reply_and_block(connection, msgQuery, -1, dbus_error);
+        dbus.dbus_message_unref(msgQuery);
+        if (dbus.dbus_error_is_set(dbus_error) != 0) {
+            std.debug.print("Connection error\n", .{});
+            dbus.dbus_error_free(dbus_error);
+            return false;
+        }
         return true;
+        // // const result: [*c]const u8 = dbus.dbus_message_get_signature(msgReply);
+        // // std.debug.print("sig = {s}", .{result});
     }
 
     fn disconnect(device_path: [*c]const u8) void {
@@ -139,15 +162,20 @@ const BLEBackend: type = struct {
     }
 
     fn subscribe() void {
-        msgQuery = dbus.dbus_message_new_method_call(
-            bluez,
-            "/org/bluez/hci0/dev_F4_12_FA_F0_91_2A/service000e/char000f",
-            "org.bluez.GattCharacteristic1",
-            "StartNotify",
-        ); // TODO: remove hardcoded path
-        _ = dbus.dbus_connection_send_with_reply_and_block(connection, msgQuery, 1000, dbus_error);
-        check(dbus_error);
-        dbus.dbus_message_unref(msgQuery);
+        for (0..50) |_| { // notifications usually take 2 seconds after connection to be available
+            msgQuery = dbus.dbus_message_new_method_call(
+                bluez,
+                "/org/bluez/hci0/dev_F4_12_FA_F0_91_2A/service000e/char000f",
+                "org.bluez.GattCharacteristic1",
+                "StartNotify",
+            ); // TODO: remove hardcoded path
+            _ = dbus.dbus_connection_send_with_reply_and_block(connection, msgQuery, 1000, dbus_error);
+            dbus.dbus_message_unref(msgQuery);
+            if (dbus.dbus_error_is_set(dbus_error) == 0) return;
+            dbus.dbus_error_free(dbus_error);
+            std.time.sleep(std.time.ns_per_ms * 100);
+        }
+        std.debug.print("Failed subscribe! Moving on without notifications\n", .{});
     }
 
     fn notifyWorker() void {
@@ -157,20 +185,29 @@ const BLEBackend: type = struct {
         dbus.dbus_bus_add_match(connection, "type='signal',path='/org/bluez/hci0/dev_F4_12_FA_F0_91_2A/service000e/char000f',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'", dbus_error); // see signals from the given interface
         dbus.dbus_connection_flush(connection);
         check(dbus_error);
+        var noDataCounter: u32 = 0;
         while (true) {
             _ = dbus.dbus_connection_read_write(connection, 0);
             msgReply = dbus.dbus_connection_pop_message(connection);
             if (msgReply == null) {
                 std.time.sleep(std.time.ns_per_ms * 5);
+                noDataCounter += 1;
+                if (noDataCounter > 200) { // 1 second without data
+                    if (!isConnected()) {
+                        _ = init();
+                        return;
+                    }
+                }
                 continue;
             }
+            noDataCounter = 0;
             var iter: dbus.DBusMessageIter = undefined;
             var sub1: dbus.DBusMessageIter = undefined;
             var sub2: dbus.DBusMessageIter = undefined;
             var sub3: dbus.DBusMessageIter = undefined;
             var sub4: dbus.DBusMessageIter = undefined;
             _ = dbus.dbus_message_iter_init(msgReply, &iter);
-            if (dbus.dbus_message_iter_get_arg_type(&iter) != dbus.DBUS_TYPE_STRING) continue;
+            if (dbus.dbus_message_iter_get_arg_type(&iter) != dbus.DBUS_TYPE_STRING) continue; // check string for disconnect
             _ = dbus.dbus_message_iter_next(&iter); // skip interface string
             if (dbus.dbus_message_iter_get_arg_type(&iter) != dbus.DBUS_TYPE_ARRAY) continue;
             dbus.dbus_message_iter_recurse(&iter, &sub1); // step into array
@@ -200,22 +237,27 @@ const BLEBackend: type = struct {
         var sub1: dbus.DBusMessageIter = undefined;
         var sub2: dbus.DBusMessageIter = undefined;
         var sub3: dbus.DBusMessageIter = undefined;
-        var readMessage: ?*dbus.DBusMessage = null;
+        var writeMessage: ?*dbus.DBusMessage = null;
         const offset: ?[*c]const u8 = "offset";
         const zero: u16 = 0;
         const sig: [*c]const u8 = "{sv}";
-        readMessage = dbus.dbus_message_new_method_call(
+        writeMessage = dbus.dbus_message_new_method_call(
             bluez,
             "/org/bluez/hci0/dev_F4_12_FA_F0_91_2A/service000e/char000f",
             "org.bluez.GattCharacteristic1",
             "WriteValue",
         ); // TODO: add string concatenation to remove hardcoded path
 
-        dbus.dbus_message_iter_init_append(readMessage, &args);
+        dbus.dbus_message_iter_init_append(writeMessage, &args);
 
         var send_data: ?[*c]u8 = @ptrCast(&tx_buf);
 
-        std.debug.print("len = {d}\n", .{tx_buf_len});
+        std.debug.print("sent {d} byte(s) : ", .{tx_buf_len});
+
+        for (0..tx_buf_len) |i| {
+            std.debug.print("{x:0>2} ", .{tx_buf[i]});
+        }
+        std.debug.print("\n", .{});
 
         // why does DBus api have to be this convoluted
         _ = dbus.dbus_message_iter_open_container(&args, dbus.DBUS_TYPE_ARRAY, dbus.DBUS_TYPE_BYTE_AS_STRING, &data);
@@ -230,21 +272,23 @@ const BLEBackend: type = struct {
         _ = dbus.dbus_message_iter_close_container(&sub1, &sub2);
         _ = dbus.dbus_message_iter_close_container(&args, &sub1);
 
-        return readMessage;
+        return writeMessage;
     }
 
     fn readNextBLEPacket(self: *Self) void {
+        if (rx_buf_len == 0) return;
         std.Thread.Mutex.lock(&rx_mutex);
         for (0..rx_buf_len) |i| {
             self.readBuffer[i] = rx_buf[i];
         }
         self.readAvailable = rx_buf_len;
+        rx_buf_len = 0;
         std.Thread.Mutex.unlock(&rx_mutex);
 
         self.readPointer = 0;
         std.debug.print(
             "received {d} byte(s) : ",
-            .{rx_buf_len},
+            .{self.readAvailable},
         );
         for (0..self.readAvailable) |i| {
             std.debug.print("{x:0>2} ", .{self.readBuffer[i]});
@@ -268,11 +312,15 @@ const BLEBackend: type = struct {
         } else {
             std.debug.print("bluetooth is off\n", .{});
         }
-        // disconnect("/org/bluez/hci0/dev_F4_12_FA_F0_91_2A");
-        scanOn();
-        dev_path = findDeviceByMac("/org/bluez/hci0/dev_F4_12_FA_F0_91_2A");
-        scanOff();
-        _ = connect(dev_path);
+        while (!isConnected()) {
+            if (!connect(dev_path)) {
+                std.debug.print("Unknown device. Scanning...\n", .{});
+                scanOn();
+                dev_path = findDeviceByMac(dev_path);
+                scanOff();
+            }
+        }
+        std.debug.print("Device connected\n", .{});
         subscribe();
         thread = std.Thread.spawn(.{}, notifyWorker, .{}) catch undefined;
         return self;
@@ -320,8 +368,6 @@ const BLEBackend: type = struct {
 
     pub fn end(self: Self) void {
         _ = self;
-        msgReply = dbus.dbus_connection_send_with_reply_and_block(connection, msgQuery, 1000, dbus_error);
-        check(dbus_error);
         disconnect(dev_path);
         dbus.dbus_message_unref(msgQuery);
         dbus.dbus_message_unref(msgReply);
