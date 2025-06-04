d079c2fc37fac498aa0fa11e0028bbb51b421f6e
Dashiell Elliott <dashiell.elliott@yahoo.com>
2025-06-04 00:44:25 -0400

    properly read and write packed struct

--- zig/src/log.zig
+++ zig/src/log.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+fn packedSize(comptime T: type) usize {
+    return @divExact(@bitSizeOf(T), 8);
+}
+
 pub const Header = packed struct {
     const Self = @This();
     git_hash: u256,
@@ -24,46 +28,67 @@ pub const Header = packed struct {
     }
 };
 
-pub const Log = struct {
-    const Self = @This();
-    file: std.fs.File,
-    header: Header,
+pub fn Log(comptime PacketTypes: type) type {
+    return struct {
+        const Field = packed struct { id: u16, size: u16 };
+        const FieldCorrection = struct { actualSize: usize };
+        const numFields = @typeInfo(PacketTypes).Struct.fields.len;
 
-    pub fn init(header: Header, comptime PacketTypes: type) !Self {
-        std.fs.cwd().access("log", .{}) catch {
-            try std.fs.cwd().makeDir("log");
-        };
+        const Self = @This();
+        file: std.fs.File,
+        header: Header,
+        fieldCorrections: [numFields]?FieldCorrection = @splat(null),
 
-        var self = Self{
-            .file = try std.fs.cwd().createFile("log/test.tl", .{}),
-            .header = header,
-        };
+        pub fn init(header: Header) !Self {
+            std.fs.cwd().access("log", .{}) catch {
+                try std.fs.cwd().makeDir("log");
+            };
 
-        try self.file.writer().writeStruct(header);
+            var self = Self{
+                .file = try std.fs.cwd().createFile("log/test.tl", .{}),
+                .header = header,
+            };
 
-        try self.file.writer().writeStruct(@as(packed struct { a: u32 }, .{ .a = @sizeOf(Header) }));
+            _ = try self.file.writer().write(&@as([packedSize(Header)]u8, @bitCast(header)));
 
-        inline for (@typeInfo(PacketTypes).Struct.fields, 0..) |packet, i| {
-            try self.file.writer().writeStruct(@as(
-                packed struct {
-                    id: u16,
-                    size: u16,
-                },
-                .{
-                    .id = i,
-                    .size = @sizeOf(packet.type),
-                },
-            ));
+            inline for (@typeInfo(PacketTypes).Struct.fields, 0..) |packet, i| {
+                try self.file.writer().writeStruct(@as(
+                    Field,
+                    .{
+                        .id = i,
+                        .size = @sizeOf(packet.type),
+                    },
+                ));
+            }
+
+            return self;
         }
 
-        return self;
-    }
+        pub fn initFromFile(filename: []const u8) !Self {
+            var self = Self{
+                .file = try std.fs.cwd().openFile(filename, .{ .read = true }),
+                .header = undefined,
+            };
 
-    pub fn log() void {
-        std.time.microTimestamp();
-    }
+            var buf: [packedSize(Header)]u8 = undefined;
+            try self.file.reader().readNoEof(&buf);
+            self.header = @bitCast(buf);
 
-    pub fn close(self: *Self) void {
-        self.file.close();
-    }
-};
+            inline for (@typeInfo(PacketTypes).Struct.fields, 0..) |packet, i| {
+                const f = try self.file.reader().readStruct(Field);
+                if (f.id != i) return error.BadHeader;
+                if (f.size != @sizeOf(packet.type)) self.fieldCorrections[i] = .{ .actualSize = f.size };
+            }
+
+            return self;
+        }
+
+        pub fn log() void {
+            std.time.microTimestamp();
+        }
+
+        pub fn close(self: *Self) void {
+            self.file.close();
+        }
+    };
+}
