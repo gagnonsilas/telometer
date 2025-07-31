8784a341ba4ae77de6308824fa7f90096e8376bb
gagnonsilas <gagnon.silas@gmail.com>
2025-07-31 09:22:39 -0400

    fix merge from fsae

--- dashboard/src/backend.zig
+++ dashboard/src/backend.zig
@@ -1,190 +1,42 @@
 const std = @import("std");
 const posix = std.posix;
+
 const MAX_UDP_PACKET_SIZE = 1024;
 const tm = @import("telometer");
 
-const telemetry = @cImport({
-    @cInclude("Packets.h");
-});
-// https://github.com/Hedwyn/canzig/blob/master/src/socketcan.zig
+pub const Backend = UDPBackend;
 
-const CanError = error{
-    SendFailed,
-    InterfaceNotFound,
-    SocketCanFailure,
-    UnknownCanID,
-};
-
-const SockaddrCan = extern struct {
-    can_family: u16 = posix.AF.CAN,
-    can_ifindex: i32,
-    data: [10]u8,
-};
-
-pub const CanFrame = extern struct {
-    id: u32,
-    len: u8,
-    bytes: [3]u8,
-
-    data: [8]u8 = undefined,
-};
-
-pub const Backend = CANBackend;
-
-const StrError = error{
-    BufferTooSmall,
-};
-
-/// Copies the characters from `input` to `output`
-/// Returns StrError if output is too small
-pub fn strcpy(input: []const u8, output: []u8) StrError!void {
-    if (input.len > output.len) {
-        std.debug.print("Input cannot fit into output", .{});
-        return StrError.BufferTooSmall;
-    }
-    for (0..input.len) |i| {
-        output[i] = input[i];
-    }
-}
-
-const CANBackend: type = struct {
+const UDPBackend: type = struct {
     const Self = @This();
-    canSocket: posix.socket_t,
-    addr: SockaddrCan,
-    frame: CanFrame,
-    data_start: usize,
 
     pub fn init() Self {
-        var backend: Self = .{
-            .canSocket = undefined,
-            .addr = undefined,
-            .frame = undefined,
-            .data_start = 0,
-        };
-        backend.openCANSocket() catch unreachable;
-        std.debug.print("huh? \n", .{});
-        return backend;
-    }
-
-    pub fn openCANSocket(self: *Self) !void {
-        self.canSocket = try posix.socket(
-            posix.AF.CAN,
-            posix.SOCK.RAW | posix.SOCK.NONBLOCK,
-            1, // CAN_RAW <linux/can.h>
-        );
-
-        var ifname = [_]u8{0} ** 16;
-        try strcpy("vcan0", &ifname); // :)
-
-        var ifreq = posix.ifreq{
-            .ifrn = .{ .name = ifname },
-            .ifru = undefined,
-        };
-
-        posix.ioctl_SIOCGIFINDEX(self.canSocket, &ifreq) catch |e| {
-            std.debug.print("ioctl reported {} when trying to get the can interface index", .{e});
-            return CanError.InterfaceNotFound;
-        };
-
-        std.debug.print("CAN interface index is {}\n", .{ifreq.ifru.ivalue});
-
-        self.addr = .{
-            .can_ifindex = ifreq.ifru.ivalue,
-            .data = undefined,
-        };
-
-        // @memset(&self.addr.data, &ifreq.ifru.ivalue);
-
-        posix.bind(self.canSocket, @ptrCast(&self.addr), @sizeOf(SockaddrCan)) catch {
-            return CanError.SocketCanFailure;
-        };
-        std.debug.print("Bound to socketcan successfully\n", .{});
-    }
-
-    pub fn connect(self: *Self, addr: posix.sockaddr) void {
-        self.addr = addr;
+        const self: Self = .{};
+        return self;
     }
 
     pub fn update(self: *Self) void {
-        _ = self; // autofix
-        // if (self.writePointer > 0) {
-        //     _ = posix.sendto(
-        //         self.canSocket,
-        //         self.writeBuffer[0..self.writePointer],
-        //         0,
-        //         &self.addr,
-        //         @sizeOf(posix.socklen_t),
-        //     ) catch {};
-        //     self.writePointer = 0;
-        // }
-
-        // self.readNextUDPPacket();
+        _ = self;
     }
 
     pub fn writePacket(self: *Self, header: tm.Header, data: tm.Data) bool {
-        _ = data; // autofix
-        _ = header; // autofix
-        _ = self; // autofix
-
-        // @memcpy(self.writeBuffer[self.writePointer .. self.writePointer + @sizeOf(tm.Header)], @as([*]u8, @constCast(@ptrCast(&header))));
-        // self.writePointer += @sizeOf(tm.Header);
-        // @memcpy(self.writeBuffer[self.writePointer .. self.writePointer + data.size], @as([*]u8, @ptrCast(data.pointer)));
-        // self.writePointer += data.size;
-        return false;
+        _ = self;
+        _ = header;
+        _ = data;
+        return true;
     }
 
     pub fn read(self: *Self, buffer: ?[*]u8, size: usize) !void {
-        if (buffer) |buf| {
-            @memcpy(
-                buf[0..size],
-                self.frame.data[0..size],
-            );
-        }
-    }
-
-    pub fn translateHeader(self: Self, id: u32) !tm.Header {
-        return switch (id) {
-            0x300 => tm.Header{ .id = 1 },
-            0x301 => tm.Header{ .id = 4 },
-            0x707 => tm.Header{ .id = 2 },
-            0x709 => tm.Header{ .id = 7 },
-            0x19107171 | 1 << 31 => tm.Header{ .id = 8 },
-            0x708 => tm.Header{ .id = 6 },
-            0x401 => tm.Header{ .id = 11 },
-            0x002 => tm.Header{ .id = 12 },
-            0 => tm.Header{ .id = 0 },
-            (0x1918FF71 | 1 << 31) => tm.Header{ .id = 5 },
-            (0x1928FF71 | 1 << 31) => tm.Header{ .id = 9 },
-            (0x704) => {
-                if (self.frame.data[0] == 3 and self.frame.data[1] == 1) {
-                    return tm.Header{ .id = 10 };
-                }
-                return CanError.UnknownCanID;
-            },
-            // 1799 => tm.Header{ .id = 2 },
-            else => CanError.UnknownCanID,
-        };
+        _ = self;
+        _ = buffer;
+        _ = size;
     }
 
     pub fn getNextHeader(self: *Self) ?tm.Header {
-        while (true) {
-            const ret = std.posix.system.recvfrom(
-                self.canSocket,
-                @ptrCast(&self.frame),
-                @sizeOf(CanFrame),
-                0,
-                null,
-                null,
-            );
-            if (ret == -1) {
-                return null;
-            }
-            // self.data_start = self.frame.data.len - self.frame.len;
-            return self.translateHeader(self.frame.id) catch continue;
-        }
+        _ = self;
+        return null;
     }
 
     pub fn end(self: Self) void {
-        posix.close(self.updSocket);
+        _ = self;
     }
 };
--- src/Packets.h
+++ src/Packets.h
@@ -4,147 +4,17 @@
 
 #include <stdbool.h>
 #define PACKETS(P, N)                                                          \
-  P(N, CarHeartBeat, MsgCarHeartbeat)                                          \
-  P(N, Throttle, ThrottlePacket)                                               \
-  P(N, Pedal, PedalInfo)                                                       \
-  P(N, PedalFault, PedalFaultInfo)                                             \
-  P(N, Brake, BrakePacket)                                                     \
-  P(N, inverterHS1, MsgInverterHS1TorqueFeedback)                              \
-  P(N, PedalCounts, CountsInfo)                                                \
-  P(N, BrakeCounts, BrakeCountsInfo)                                           \
-  P(N, inverterHC1, MsgInverterHC1Demands)                                     \
-  P(N, inverterHD1, MsgInverterHD1DebugCurrents)                               \
-  P(N, Voltage, MsgVoltageResponse)                               \
-  P(N, MinMax, MsgCarVoltageDistribution)                               \
-  P(N, AMS, TelemPacket)                               \
-  P(N, IMU_PITCH, TelemPacket)
+  P(N, enabled, bool)                                                          \
+  P(N, state, uint16_t)                                                        \
+  P(N, controlmode, uint16_t)                                                  \
+  P(N, time, double)
 
-// P(N, IMU_ROLL, TelemPacket)                                                  \
-  // P(N, IMU_X, TelemPacket)                                                     \
-  // P(N, IMU_y, TelemPacket)                                                     \
-  // P(N, IMU_Z, TelemPacket)                                                     \
-  // P(N, MAG_X, TelemPacket)                                                     \
-  // P(N, MAG_Y, TelemPacket)                                                     \
-  // P(N, MAG_Z, TelemPacket)                                                     \
-  // P(N, VBUS, TelemPacket)                                                      \
-  // P(N, IBUS, TelemPacket)                                                      \
-  // P(N, MOTOR_RPM, TelemPacket)                                                 \
-  // P(N, GPS_LAT, TelemPacket)                                                   \
-  // P(N, GPS_LON, TelemPacket)                                                   \
-  // P(N, STEERING_WHEEL_ANGLE, TelemPacket)                                      \
-  // P(N, SUSPENSION_FR, TelemPacket)                                             \
-  // P(N, SUSPENSION_FL, TelemPacket)                                             \
-  // P(N, SUSPENSION_BR, TelemPacket)                                             \
-  // P(N, SUSPENSION_BL, TelemPacket)                                             \
-  // P(N, WHEELSPEED_FR, TelemPacket)                                             \
-  // P(N, WHEELSPEED_FL, TelemPacket)                                             \
-  // P(N, WHEELSPEED_BR, TelemPacket)                                             \
-  // P(N, WHEELSPEED_BL, TelemPacket)                                             \
-  // P(N, DRIVER_HEARTRATE, TelemPacket)                                          \
-  // P(N, PUMP_SPEED, TelemPacket)
-// #define ID(namespace, id) (namespace << 8 | id)
-// #define ThrottleID 768
-
-typedef struct __attribute__((__packed__)) ThrottlePacket {
-  uint32_t throttle;
-  uint8_t flags;
-} ThrottlePacket;
-
-typedef struct Brake {
-  uint32_t brake1;
-  uint32_t brake2;
-} BrakePacket;
-
-typedef struct testPacket {
-  uint8_t data[8];
-} testPacket;
-
-typedef struct TelemPacket {
-  uint16_t data[4];
-} TelemPacket;
-
-typedef struct __attribute__((__packed__)) MsgInverterHS1TorqueFeedback {
-  int16_t torqueMeasured;
-  int16_t speedMeasured;
-  int16_t dcLinkCurrent;
-  uint8_t checksum;
-  uint8_t seqCounter;
-} MsgInverterHS1TorqueFeedback;
-
-// typedef uint8_t bool;
-
-typedef struct __attribute__((__packed__)) PedalInfo {
-  int16_t pct_one;
-  int16_t pct_two;
-  bool limit_sw;
-  uint8_t faults;
-  uint16_t output;
-} PedalInfo;
-
-typedef struct __attribute__((__packed__)) PedalFaultInfo {
-  uint16_t value;
-  uint16_t a;
-  uint16_t b;
-  uint8_t flags;
-  uint8_t pedal;
-} PedalFaultInfo;
-
-typedef struct __attribute__((__packed__)) CountsInfo {
-  uint16_t counts1;
-  uint16_t counts2;
-  uint16_t zero1;
-  uint16_t zero2;
-} CountsInfo;
 
 typedef struct newStruct {
   float bob;
   uint32_t color;
 } newStruct;
 
-typedef struct __attribute__((__packed__)) BrakeCountsInfo {
-  uint16_t counts_one;
-  uint16_t counts_two;
-  uint16_t zero_one;
-  uint16_t zero_two;
-} BrakeCountsInfo;
-
-typedef struct __attribute__((__packed__)) MsgInverterHC1Demands {
-  int16_t torqueRequest;
-  uint16_t controlWord;
-  int16_t torqueLimit;
-  uint8_t checksum;
-  uint8_t seqCounter;
-} MsgInverterHC1Demands;
-typedef struct __attribute__((__packed__)) MsgInverterHD1DebugCurrents {
-  int16_t Iq_ref;
-  int16_t Id_ref;
-  int16_t Iq_act;
-  int16_t Id_act;
-} MsgInverterHD1DebugCurrents;
-
-enum CarStatus : uint8_t { OK = 0, NOT_OK, DRIVER_DECEASED, ON_FIRE };
-
-typedef struct __attribute__((__packed__)) MsgCarHeartbeat {
-  uint32_t tick;
-  uint8_t status;
-  bool rtd_status;
-  uint8_t errno_;
-  uint8_t caught;
-} MsgCarHeartbeat;
-
-  
-typedef struct __attribute__((__packed__)) MsgVoltageResponse {
-    uint8_t segmentID;
-    uint8_t index;
-    uint16_t voltages[3];
-} MsgVoltageResponse;
-
-typedef struct __attribute__((__packed__)) MsgCarVoltageDistribution {
-    uint16_t min_voltage;
-    uint16_t max_voltage;
-    uint16_t avg_voltage;
-    uint16_t current;
-} MsgCarVoltageDistribution;
 
 TELOMETER_INSTANCE(Telemetry, PACKETS)
 
